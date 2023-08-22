70056d63191a662f507999947a1edb002a4d503a
Krishna <krishna@findcoder.io>
2023-08-22 16:22:26 +0530

    transiition

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "trans-bottom": {
+          "0%,100%": { transform: "translateY(50px)" },
+          "50%": { transform: "translateY(0px)" },
+        },
+        "trans-top": {
+          "0%,100%": { transform: "translateY(50px)" },
+          "50%": { transform: "translateY(0px)" },
+        },
+      },
+      animation: {
+        "trans-bottom": "trans-bottom 5s ease-in-out infinite",
+        "trans-top": "trans-bottom 2.5s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
