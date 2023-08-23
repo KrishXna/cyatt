8a9b05ddb41de37d1f44ed2a2c66e61f3e5479b7
Krishna <krishna@findcoder.io>
2023-08-23 10:47:16 +0530

    slider fixed

--- script.js
+++ script.js
@@ -42,6 +42,11 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   autoplay: {
     delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // If we need pagination
